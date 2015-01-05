1909cc620393d57d55cceb0e3263de85d71805b2
Farrukh Askari <farrukh.askari01@gmail.com>
2015-01-05 11:57:29 +0500

    MVC try

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int clas)
+        {
+            ViewBag.Name = name;
+            ViewBag.Clas = clas;
+            return View();
+        }
+
+        public string Add(int a = 0, int b = 0)
+        {
+            return HttpUtility.HtmlEncode("The sum is: "+(a+b));
+        }
+
+        public string Info(string name = null, int age = 0)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", you are " + age + " years old.");
+        }
+    }
+}
